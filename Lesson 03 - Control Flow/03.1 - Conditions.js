c795c2c6e11a347696403c6a90552ff6d1bee1cf
Maruf Rahman <117238487+Maruf2k3@users.noreply.github.com>
2024-07-20 01:01:44 +0400

    Second Commit

---
+++ Lesson 03 - Control Flow/03.1 - Conditions.js
@@ -0,0 +1,14 @@
+//----------- Consitionals ---------
+
+let age = 19;
+
+if(age >= 18){
+    console.log(`You are ${age} years old and you are now and adult`)
+}
+
+else if(age <= 18){
+    console.log(`You are ${age} years old and you are not an adult`)
+}
+else{
+    console.log("Invalid Input!")
+}
